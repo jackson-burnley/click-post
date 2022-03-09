aab5d9aae87443cc749c3c41d53ca71a03f62ba6
Jack <jacktech.system@gmail.com>
2022-03-09 11:53:21 -0300

    Add asyncstorage and catch post infos

--- src/screen/newComment/index.tsx
+++ src/screen/newComment/index.tsx
@@ -1,18 +1,81 @@
 import React from "react";
-import { View, Text, TextInput, Button } from "react-native";
-import { TextTitle } from "../../components/Header/styles";
-import { Label, SmallInput, LargeInput } from "./styles";
+import {
+  View,
+  Text,
+  TextInput,
+  Button,
+  TouchableOpacity,
+  Keyboard,
+  Alert,
+} from "react-native";
+import {
+  Content,
+  Label,
+  SmallInput,
+  LargeInput,
+  TextComment,
+  ButtonSubmit,
+  TextSubmit,
+  ViewForm,
+} from "./styles";
+import { AntDesign } from "@expo/vector-icons";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const NewComment = (props: any) => {
+  const [name, setName] = React.useState("");
+  const [title, setTitle] = React.useState("");
+  const [post, setPost] = React.useState("");
+  const [counter, setCounter] = React.useState("1");
+
+  const asyncReq = async () => {
+    try {
+      if (name && post) {
+        await AsyncStorage.setItem(
+          counter,
+          JSON.stringify({ name, post, title })
+        );
+        setCounter(String(Number(counter) + 1));
+        Keyboard.dismiss();
+        Alert.alert(
+          `Seu nome é: ${name} com titulo ${title} e sua postagem é ${post}`
+        );
+        setName("");
+        setPost("");
+        setTitle("");
+      }
+    } catch {
+      Alert.alert("Carregando...");
+    }
+  };
+
   return (
-    <View>
-      <TextTitle>Escreva sua postagem</TextTitle>
-      <Label>Seu nome</Label>
-      <SmallInput />
-      <Label>Postagem</Label>
-      <LargeInput />
-      <Button title="Submeter" onPress={() => {}} />
-    </View>
+    <Content>
+      <AntDesign name="arrowleft" size={28} color="black" />
+
+      <ViewForm>
+        <TextComment>Escreva sua postagem</TextComment>
+        <Label>Seu nome</Label>
+        <SmallInput
+          placeholder="Digite aqui"
+          onChangeText={(newText) => setName(newText)}
+          defaultValue={name}
+        />
+        <Label>Título da postagem</Label>
+        <SmallInput
+          placeholder="Digite aqui"
+          onChangeText={(newText) => setTitle(newText)}
+          defaultValue={title}
+        />
+        <Label>Postagem</Label>
+        <LargeInput
+          onChangeText={(newPost) => setPost(newPost)}
+          defaultValue={post}
+        />
+        <ButtonSubmit>
+          <TextSubmit onPress={asyncReq}>Submeter</TextSubmit>
+        </ButtonSubmit>
+      </ViewForm>
+    </Content>
   );
 };
 
--- src/screen/newComment/styles.ts
+++ src/screen/newComment/styles.ts
@@ -1,19 +1,57 @@
 import styled from "styled-components/native";
-import { TextInput } from "react-native";
+import { TextInput, TouchableOpacity } from "react-native";
+
+export const Content = styled.View`
+  margin-top: 30px;
+  margin-left: 14px;
+  flex-direction: row;
+`;
+
+export const ButtonSubmit = styled.TouchableOpacity`
+  border-radius: 10px;
+  background-color: #33acab;
+  width: 250px;
+  height: 40px;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const ViewForm = styled.View`
+  margin-top: 50px;
+`;
+
+export const TextSubmit = styled.Text`
+  color: #fff;
+  font-weight: bold;
+  font-size: 16px;
+`;
+
+export const TextComment = styled.Text`
+  font-size: 24px;
+  font-weight: bold;
+  color: #33acab;
+`;
 
 export const Label = styled.Text`
   font-size: 24px;
   font-weight: 500;
+  margin-bottom: 5px;
+  margin-top: 10px;
 `;
 
 export const SmallInput = styled.TextInput`
   width: 340px;
   height: 50px;
   border: 3px solid #4f388b;
+  padding: 15px;
+  font-size: 20px;
 `;
 
 export const LargeInput = styled.TextInput`
   width: 340px;
-  height: 210px;
+  height: 310px;
+  margin-bottom: 20px;
   border: 3px solid #4f388b;
+  padding: 15px;
+  font-size: 20px;
 `;
